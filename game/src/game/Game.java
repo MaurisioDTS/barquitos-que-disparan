0ffbe14c241736de7a2c7b18a93e6560308d5eac
MaurisioDTS <MaurisioDTS@gmail.com>
2024-02-28 23:59:37 +0100

    pruebas en la vista y en el control: etiquetas, botónes e inserción de audio

--- game/src/game/Game.java
+++ game/src/game/Game.java
@@ -11,25 +11,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Objects;
+
 public class Game extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("View.fxml")));
         
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
     }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
--- game/src/game/ViewController.java
+++ game/src/game/ViewController.java
@@ -1,35 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package game;
 
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.media.AudioClip;
 
-/**
- *
- * @author Usuario
- */
 public class ViewController implements Initializable {
-    
+    @FXML
+    public Button button;
     @FXML
     private Label label;
-    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    ProgressBar progress = new ProgressBar(0.0);
+    //AudioClip intro = new AudioClip("res/audio/test.mp3");
+
+    @FXML
+
+    // 5 horas y 47 minutos para esta mierda que nisiquiera funciona bien.
+
+    private void handleButtonAction(ActionEvent Event) throws Exception {
+        System.out.println("y pa activar a lo anormale");
+        button.setVisible(false);
+        progress.setVisible(true);
+
+         for (double i : new double[]{0.25, 0.5, 0.75,1.0}) {
+             Thread.sleep(1000);
+             progress.setProgress(i);
+         }
+        /*for(double i=1.0;i>0.0;i=i-0.1){
+
+            System.out.println(progress.getOpacity());
+            progress.setOpacity(i);
+            Thread.sleep(1000);
+        }*/
+
+        label.setText("Barquitos que disparan!!1!1!!!!");
+        //intro.play();
     }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
 }
