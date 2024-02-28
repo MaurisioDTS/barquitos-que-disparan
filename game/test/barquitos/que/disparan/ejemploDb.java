16e9bc2994a9385c54a0abccec00b9a6c31dd358
MaurisioDTS <96304957+MaurisioDTS@users.noreply.github.com>
2024-02-28 13:35:50 +0100

    Add files via upload
    Signed-off-by: MaurisioDTS <96304957+MaurisioDTS@users.noreply.github.com>

---
+++ game/test/barquitos/que/disparan/ejemploDb.java
@@ -0,0 +1,16 @@
+package barquitos.que.disparan;
+import utilidades.bbdd.Bd;
+import utilidades.bbdd.Gestor_conexion_POSTGRE;
+
+public class ejemploDb {
+    public static void main(String[] args) {
+        // TODO code application logic here
+            Gestor_conexion_POSTGRE gestor=new Gestor_conexion_POSTGRE("mdddb", true);
+            
+            String consulta;
+            consulta ="select * from Usuario";
+            
+            System.out.println(Bd.consultaModificacion(gestor,consulta));    
+            gestor.cerrar_Conexion(true);
+    }
+}
---
+++ game/test/barquitos/que/disparan/hash.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barquitos.que.disparan;
+/**
+ *
+ * @author Usuario
+ */
+public class hash {
+    public static void main(String[] args){
+    String password="1234";
+    
+    String sha256hex = org.apache.commons.codec.digest.DigestUtils.sha256Hex(password);  
+    
+    System.out.println(sha256hex);
+    }
+}
