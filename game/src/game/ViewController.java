9797eeb4e18ab7b44e0edbca608d14ec9f96bbc4
Usuario <Usuario@A3PC04>
2024-03-11 11:02:01 +0100

    skefhs

--- game/src/game/ViewController.java
+++ game/src/game/ViewController.java
@@ -1,45 +1,39 @@
 package game;
 
+import java.io.File;
 import java.net.URL;
-import java.util.Objects;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.media.AudioClip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class ViewController implements Initializable {
-    @FXML
-    public Button button;
-    @FXML
-    private Label label;
-    @FXML
-    ProgressBar progress = new ProgressBar(0.0);
-    //AudioClip intro = new AudioClip("res/audio/test.mp3");
 
     @FXML
-
+    Media intro = new Media(Paths.get("res/audio/test.mp3").toUri().toString());
+    MediaPlayer m=new MediaPlayer(intro);
+    
+    
+    @FXML
+    private void stop(ActionEvent Event){
+        System.out.println("Exiting...");
+        System.exit(0);
+    }
     // 5 horas y 47 minutos para esta mierda que nisiquiera funciona bien.
-
-    private void handleButtonAction(ActionEvent Event) throws Exception {
-        System.out.println("y pa activar a lo anormale");
-        button.setVisible(false);
-        progress.setVisible(true);
-
-         for (double i : new double[]{0.25, 0.5, 0.75,1.0}) {
-             Thread.sleep(1000);
-             progress.setProgress(i);
-         }
-        /*for(double i=1.0;i>0.0;i=i-0.1){
-
-            System.out.println(progress.getOpacity());
-            progress.setOpacity(i);
-            Thread.sleep(1000);
-        }*/
-
-        label.setText("Barquitos que disparan!!1!1!!!!");
-        //intro.play();
+    @FXML
+    private void battle(ActionEvent Event){
+        System.out.println("Battle");
+        m.play();
+    }
+    @FXML
+    private void settings(ActionEvent Event){
+        System.out.println("Settings");
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
