e5c8475cf780896281694207d0c5b0b2eb51329d
Globoomg <121044392+Globoomg@users.noreply.github.com>
2024-03-22 12:07:27 +0100

    Añadido metodo provisional a la clase board

--- game/src/game/Board.java
+++ game/src/game/Board.java
@@ -10,5 +10,27 @@ package game;
  * @author Usuario
  */
 public class Board {
+    private int [][] board = new int [10][10]; 
+    //Metodo que compruebe que haya barco, agua e impactado. -1-agua, 0-aguaImpactado, 1-barco, 2-barco impactado.
+    //public boolean checkPosition(int[][] coords){
+    //}
     
+    //Metodo provisional
+    public int checkBo(int row, int column) {
+        if (row < 0 || row >= board.length || column < 0 || column >= board[0].length) {
+            return -1; // celda inválida
+        }
+
+        int celda = board[row][column];
+
+        if (celda == -1) {
+            return 0; // agua
+        } else if (celda == 1) {
+            return 2; // barco
+        } else if (celda == 2) {
+            return 3; // barco impactado
+        } else {
+            return -1; // celda inválida
+        }
+    }
 }
