60c862b48124815f122cbf7c07c428de8d1db6ea
Daniel-Eixea <161595241+Daniel-Eixea@users.noreply.github.com>
2024-04-16 13:19:30 +0200

    Arreglado NullPointerException de la clase draggable

--- game/src/game/DraggableMaker.java
+++ game/src/game/DraggableMaker.java
@@ -9,15 +9,16 @@ public class DraggableMaker {
     private double mouseAnchorY;
 
     public void makeDraggable(Node node){
-
-        node.setOnMousePressed(mouseEvent -> {
-            mouseAnchorX = mouseEvent.getX();
-            mouseAnchorY = mouseEvent.getY();
-        });
-
-        node.setOnMouseDragged(mouseEvent -> {
-            node.setLayoutX(mouseEvent.getSceneX() - mouseAnchorX);
-            node.setLayoutY(mouseEvent.getSceneY() - mouseAnchorY);
-        });
+        if (node != null) {
+            node.setOnMousePressed(mouseEvent -> {
+                mouseAnchorX = mouseEvent.getX();
+                mouseAnchorY = mouseEvent.getY();
+            });
+        
+            node.setOnMouseDragged(mouseEvent -> {
+                node.setLayoutX(mouseEvent.getSceneX() - mouseAnchorX);
+                node.setLayoutY(mouseEvent.getSceneY() - mouseAnchorY);
+            });
+        }
     }
 }
