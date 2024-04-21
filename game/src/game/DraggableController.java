a38ad2be1ef3f67cb05f9615f35e16fac4d5fb98
Globoomg <121044392+Globoomg@users.noreply.github.com>
2024-04-21 18:51:17 +0200

    Cambio de codigo DraggableController y Final

--- game/src/game/DraggableController.java
+++ game/src/game/DraggableController.java
@@ -11,7 +11,10 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+
+import javax.swing.text.html.ImageView;
 
 public class DraggableController{
 
@@ -21,6 +24,8 @@ public class DraggableController{
     private HBox currentDraggedBox;
     @FXML
     private boolean rotateKeyPressed = false;
+    @FXML
+    private GridPane boardP1;
     @FXML
     private HBox shipLength1;
     @FXML
@@ -33,7 +38,38 @@ public class DraggableController{
     private HBox shipLength5;
     @FXML
     private DraggableMaker draggableMaker;
-    
+    @FXML
+    private HBox draggedBox;
+
+    @FXML
+    private void onMousePressed(MouseEvent event){
+        if (event.getSource() instanceof HBox){
+            draggedBox = (HBox) event.getSource();
+        }
+    }
+
+    @FXML
+    private void onMouseDragged(MouseEvent event){
+        if (draggedBox != null){
+            draggedBox.setTranslateX(event.getSceneX());
+            draggedBox.setTranslateY(event.getSceneY());
+        }
+    }
+
+    @FXML
+    private void onMouseReleased(MouseEvent event){
+        if (draggedBox != null){
+            double x = event.getX();
+            double y = event.getY();
+            int column = (int) (x / (boardP1.getWidth()/10));
+            int row = (int) (y / (boardP1.getWidth()/10));
+            GridPane.setConstraints(draggedBox, column, row);
+            boardP1.getChildren().add(draggedBox);
+            draggedBox.setTranslateX(0);
+            draggedBox.setTranslateY(0);
+            draggedBox = null;
+        }
+    }
     Board brd=new Board("elpepe");
     
     // changes in scene
--- game/src/game/FinalController.java
+++ game/src/game/FinalController.java
@@ -7,7 +7,11 @@ package game;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +19,18 @@ import javafx.fxml.Initializable;
  * @author Usuario
  */
 public class FinalController implements Initializable {
+
+    @FXML
+    private Label lblWinner;
+
+    @FXML
+    private Label lblWarshipsDestroyed;
+
+    @FXML
+    private TextField tfUser;
+
+    @FXML
+    private TextField tfComments;
 
     /**
      * Initializes the controller class.
